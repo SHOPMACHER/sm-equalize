53e633d28a4e5b565ad6dcd04c8c1f72f41dfdda
Dennis Stockhofe <d.stockhofe@shopmacher.de>
2017-11-29 12:17:03 +0100

    adds reset param to reset height before setting new height, updates version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const has = Object.prototype.hasOwnProperty;
 
+let reset = null;
+
 /**
  * Iterates over an array of DOM elements, determines the highest element and applies its height to
  * all elements.
@@ -7,6 +9,10 @@ const has = Object.prototype.hasOwnProperty;
  * @param group Array of elements
  */
 const equalize = (group) => {
+    if (reset) {
+        group.forEach($el => $el.style.height = '');
+    }
+
     const maxHeight = Math.max.apply(undefined, group.map($el => $el.getBoundingClientRect().height));
 
     group.forEach($el => $el.style.height = `${maxHeight}px`);
@@ -16,9 +22,10 @@ const equalize = (group) => {
  * Initializes the equalizer based on a DOM root node and groups the respective elements by their given
  * data-equalize value.
  *
+ * @param resetElements Resets the height to initial height
  * @param $root Root element to start equalizing from
  */
-export default ($root = document) => {
+export default (resetElements = false, $root = document) => {
     const $elements = [...$root.querySelectorAll('[data-equalize]')];
     const groups = $elements.reduce((result, $element) => {
         const key = $element.getAttribute('data-equalize');
@@ -29,6 +36,8 @@ export default ($root = document) => {
 
         return result;
     }, {});
+
+    reset = resetElements;
 
     Object.keys(groups).forEach(key => equalize(groups[key]));
 };
